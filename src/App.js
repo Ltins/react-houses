43a08e4c47905d87c9af1f0b32bc0ab60043d11b
Ltins <adondenko@bk.ru>
2023-11-01 01:44:51 +0300

    Removed header borders

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import classes from "./App.module.css";
 import HousesList from "./components/house/HousesList";
 import Form from "./components/Form";
 
-/*
-Must have:
--Check for bugs + refactor code
--House redesign
--Change header styling
-Maybe:
--Try to implement rendering via url params
-*/
-
 function App() {
   return (
     <div className={classes["house-constructor"]}>
