dbd595edcdd7b91205d32502f3074198757836b8
Ltins <adondenko@bk.ru>
2023-11-21 13:16:58 +0300

    Got rid of reduntant component reevaluation cycles

--- src/components/house/House.js
+++ src/components/house/House.js
@@ -1,8 +1,10 @@
+import { memo } from "react";
+
 import Entrance from "./house-parts/Entrance";
 import Roof from "./house-parts/Roof";
 import Floor from "./house-parts/Floor";
 
-const House = (props) => {
+const House = memo((props) => {
   let floors = [];
   for (let i = 0; i < props.floors - 1; i++) {
     floors.push(<Floor color={props.color} key={`${props.id}${i}`} />);
@@ -14,6 +16,6 @@ const House = (props) => {
       <Entrance color={props.color} />
     </div>
   );
-};
+});
 
 export default House;
